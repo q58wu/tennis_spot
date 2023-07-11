9c720d05bd598467ce8689a7c8d521ca6637cae0
Robin Wu <robinwu1224@gmail.com>
2023-07-10 23:39:06 -0400

    address issues

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 import time
+from datetime import datetime
 
 
 class CourtType(Enum):
@@ -25,6 +26,7 @@ class Day(Enum):
     Sat = "Sat"
     Sun = "Sun"
 
+
 ## parameters:
 user_name = "5012978"
 pass_word = "795376"
@@ -33,7 +35,7 @@ user_name1 = "4889740"
 pass_word1 = "150200"
 
 desired_time_slot_map = {Day.Mon: "9:00AM", Day.Tue: "9:00AM", Day.Wed: "9:00AM", Day.Thu: "9:00AM", Day.Fri: "9:00AM",
-                  Day.Sat: "9:00AM", Day.Sun: "9:00AM" }
+                         Day.Sat: "9:00AM", Day.Sun: "9:00AM"}
 
 ## constants
 day_indent_map = {"Mon": 2, "Tue": 3, "Wed": 4, "Thu": 5, "Fri": 6,
@@ -42,42 +44,59 @@ day_indent_map = {"Mon": 2, "Tue": 3, "Wed": 4, "Thu": 5, "Fri": 6,
 this_week_status = {"Mon": False, "Tue": False, "Wed": False, "Thu": False, "Fri": False,
                     "Sat": False, "Sun": False}
 
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+tennis_url = "https://efun.toronto.ca/torontofun/Activities/ActivitiesAdvSearch.asp?invocationLink=ALTLINK&SectionId=119&SubSectionId=229&BasicSearch=True"
+driver = webdriver.Chrome(chrome_options)
+## //*[@id="no-search-result-div"]
+
 ## TODO implement this
 def check_if_next_week_is_available():
     return True
 
+
 def login():
     driver.get("https://efun.toronto.ca/torontofun/MyAccount/MyAccountUserLogin.asp?Referrer=")
-    login = driver.find_element(By.ID, "user-login")
-    client_number_field = \
-        login.find_element(By.TAG_NAME, "table").find_element(By.TAG_NAME, "tr").find_elements(By.TAG_NAME, "td")[
-            1].find_element(By.TAG_NAME, "input")
-    password_field = \
-        login.find_element(By.TAG_NAME, "table").find_elements(By.TAG_NAME, "tr")[1].find_elements(By.TAG_NAME, "td")[
-            1].find_element(By.TAG_NAME, "input")
-    log_in_button = \
-        login.find_element(By.TAG_NAME, "table").find_elements(By.TAG_NAME, "tr")[3].find_elements(By.TAG_NAME, "td")[
-            0].find_element(By.TAG_NAME, "input")
+    try:
+        login = driver.find_element(By.ID, "user-login")
+        client_number_field = \
+            login.find_element(By.TAG_NAME, "table").find_element(By.TAG_NAME, "tr").find_elements(By.TAG_NAME, "td")[
+                1].find_element(By.TAG_NAME, "input")
+        password_field = \
+            login.find_element(By.TAG_NAME, "table").find_elements(By.TAG_NAME, "tr")[1].find_elements(By.TAG_NAME, "td")[
+                1].find_element(By.TAG_NAME, "input")
+        log_in_button = \
+            login.find_element(By.TAG_NAME, "table").find_elements(By.TAG_NAME, "tr")[3].find_elements(By.TAG_NAME, "td")[
+                0].find_element(By.TAG_NAME, "input")
 
-    client_number_field.send_keys(user_name)
-    password_field.send_keys(pass_word)
-    log_in_button.click()
+        client_number_field.send_keys(user_name)
+        password_field.send_keys(pass_word)
+        log_in_button.click()
+    except NoSuchElementException:
+        print("already logged in")
+
 
 def filter_on_day(day: str):
     clear_filter()
-    day_check_box = driver.find_element(By.XPATH, "//*[@id='days-panel']/table/tbody/tr[2]/td[%i]/input" % day_indent_map[day])
+    day_check_box = driver.find_element(By.XPATH,
+                                        "//*[@id='days-panel']/table/tbody/tr[2]/td[%i]/input" % day_indent_map[day])
     if not day_check_box.is_selected():
         day_check_box.click()
 
+
 def unfilter_on_day(day: str):
-    day_check_box = driver.find_element(By.XPATH, "//*[@id='days-panel']/table/tbody/tr[2]/td[%i]/input" % day_indent_map[day])
+    day_check_box = driver.find_element(By.XPATH,
+                                        "//*[@id='days-panel']/table/tbody/tr[2]/td[%i]/input" % day_indent_map[day])
     if day_check_box.is_selected():
         day_check_box.click()
 
+
 def filter_on_day_and_search(day: str):
     filter_on_day(day)
+    time.sleep(2)
     search = driver.find_element(By.XPATH, "//*[@id='adv-search-buttons']/span[2]/span/input")
     search.click()
+
 
 def clear_filter():
     unfilter_on_day(Day.Mon.value)
@@ -87,6 +106,7 @@ def clear_filter():
     unfilter_on_day(Day.Fri.value)
     unfilter_on_day(Day.Sat.value)
     unfilter_on_day(Day.Sun.value)
+
 
 def filter_on_next_week():
     filter_on_day(Day.Mon.value)
@@ -99,6 +119,7 @@ def filter_on_next_week():
     search = driver.find_element(By.XPATH, "//*[@id='adv-search-buttons']/span[2]/span/input")
     search.click()
 
+
 ## start search
 ## make sure user is on page that tennis is selected and detailed table is shown
 
@@ -108,6 +129,12 @@ def filter_on_next_week():
 def try_add_to_cart(day: Day, desired_time_slot: str, desired_court_type: CourtType) -> bool:
     filter_on_day_and_search(day.value)
     time.sleep(4)
+    try:
+        driver.find_element(By.CLASS_NAME, "no-search-result-div")
+        print("No such elements")
+        return False
+    except NoSuchElementException:
+        print("Searching results...")
     drop_in_tennis = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.XPATH, "//*[@id='activity-1-20387']/div[1]/span[1]/a")))
     drop_in_tennis.click()
@@ -122,7 +149,8 @@ def try_add_to_cart(day: Day, desired_time_slot: str, desired_court_type: CourtT
                 continue
             day_text = driver.find_element(By.XPATH, "//*[@id='activity-detail-table']/div/table/tbody/tr[%s]/td[3]"
                                            % current_index)
-            time_slot_text = driver.find_element(By.XPATH, "//*[@id='activity-detail-table']/div/table/tbody/tr[%s]/td[4]"
+            time_slot_text = driver.find_element(By.XPATH,
+                                                 "//*[@id='activity-detail-table']/div/table/tbody/tr[%s]/td[4]"
                                                  % current_index)
             court_name_text = driver.find_element(
                 By.XPATH,
@@ -144,6 +172,8 @@ def try_add_to_cart(day: Day, desired_time_slot: str, desired_court_type: CourtT
             time.sleep(4)
         else:
             return False
+
+
 ## //*[@id="activity-detail-table"]/div/div[2]/div[2]/a[3]
 def select_candidate_name_and_add_to_cart_continue_shopping():
     try:
@@ -155,46 +185,35 @@ def select_candidate_name_and_add_to_cart_continue_shopping():
         driver.find_element(By.XPATH, "//*[@id='cancel']").click()
     except UnexpectedAlertPresentException:
         time.sleep(4)
-        driver.find_element(By.XPATH, "/html/body/div[1]/div[2]/form/div[1]/div[1]/table[1]/tbody/tr/td[2]/span/input").click()
+        driver.find_element(By.XPATH,
+                            "/html/body/div[1]/div[2]/form/div[1]/div[1]/table[1]/tbody/tr/td[2]/span/input").click()
         time.sleep(2)
         driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/a").click()
 
 
-chrome_options = Options()
-chrome_options.add_experimental_option("detach", True)
+def main():
+    ## first login
+    login()
 
-tennis_url = "https://efun.toronto.ca/torontofun/Activities/ActivitiesAdvSearch.asp?invocationLink=ALTLINK#top"
+    ## check if next week is ready or not
 
-driver = webdriver.Chrome(chrome_options)
+    driver.get(tennis_url)
 
-## first login
-login()
+    for key in desired_time_slot_map:
+        try_add_to_cart(key, desired_time_slot_map[key], CourtType.Bubble)
 
-## check if next week is ready or not
-
-driver.get(
-    "https://efun.toronto.ca/torontofun/Activities/ActivitiesAdvSearch.asp?invocationLink=ALTLINK&SectionId=119&SubSectionId=229&BasicSearch=True")
-
-last_button_xpath = "//*[@id='activity-detail-table']/div/div[2]/div[2]/a[4]"
-next_button_xpath = "//*[@id='activity-detail-table']/div/div[2]/div[2]/a[3]"
-sheet_body_xpath = "//*[@id='activity-detail-table']/div/table/tbody"
-
-
-## todo make sure id is correct here
-
-'''next_button = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.XPATH, "//*[@id='activity-detail-table']/div/div[2]/div[2]/a[3]")))
+    driver.get("https://efun.toronto.ca/torontofun/MyBasket/MyBasketCheckout.asp")
+    try:
+        driver.find_element(By.XPATH, "//*[@id='completeTransactionButton']").click()
+        time.sleep(4)
+        driver.find_element(By.XPATH, "//*[@id='program-liability-waiver']/form/div[2]/span[1]/input").click()
+    except NoSuchElementException:
+        print("cart is empty, cannot do anything...")
 
 
-
-time.sleep(4)
-next_button.click()
-time.sleep(4)'''
-for key in desired_time_slot_map:
-    try_add_to_cart(key, desired_time_slot_map[key], CourtType.Bubble)
-
-driver.get("https://efun.toronto.ca/torontofun/MyBasket/MyBasketCheckout.asp")
-driver.find_element(By.XPATH, "//*[@id='completeTransactionButton']").click()
-time.sleep(4)
-driver.find_element(By.XPATH, "//*[@id='program-liability-waiver']/form/div[2]/span[1]/input").click()
-
+## mins
+interval = 5
+if __name__ == "__main__":
+    while True:
+        main()
+        time.sleep(interval * 60)
